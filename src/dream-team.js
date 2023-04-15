df3508e20f61212125d2bb3bd3fa83d102d132ae
WidthDoctor <elskel1812@gmail.com>
2023-04-15 14:03:56 +0200

    feat: add solution (Сб, 15 апр 2023 14:03:55)

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,21 +1,15 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NodeList } = require('../extensions/index.js');
 
-/**
- * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
- * @return {String | Boolean} name of the team or false
- * in case of incorrect members
- *
- * @example
- * 
- * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
- * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
- *
- */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  return members
+    .filter((elem) => typeof elem === "string")//вышвыриваем не строки
+    .map((elem) => elem.trim()[0].toUpperCase())//в первую букву
+    .sort()
+    .join("");
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -1,19 +1,17 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NodeList } = require('../extensions/index.js');
 
-/**
- * Extract season from given date and expose the enemy scout!
- * 
- * @param {Date | FakeDate} date real or fake date
- * @returns {String} time of the year
- * 
- * @example
- * 
- * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
- */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+  if (typeof date.getMonth === 'undefined'||Object.keys(date).length > 0||!(date instanceof Date)) {
+    throw Error('Invalid date!');
+  }
+  const month = date.getMonth();
+  return (month === 11 || month === 0 || month === 1) ? 'winter' :
+(month === 2 || month === 3 || month === 4) ? 'spring' :
+(month === 5 || month === 6 || month === 7) ? 'summer' :
+'autumn';
 }
 
 module.exports = {
